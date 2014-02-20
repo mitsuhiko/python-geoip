3f3a63a16738502f8a9326aca26ebe6ff14ad5b4
Armin Ronacher <armin.ronacher@active-4.com>
2014-02-20 19:34:15 +0600

    Fixed a typo

--- geolite2/setup.py
+++ geolite2/setup.py
@@ -14,8 +14,8 @@ setup(
     url='http://github.com/mitsuhiko/python-geoip',
     packages=['_geoip_geolite2'],
     description='Provides access to the geolite2 database.  This product '
-        'includes GeoLite2 data created by MaxMind, available from ',
-        'http://www.maxmind.com/'
+        'includes GeoLite2 data created by MaxMind, available from '
+        'http://www.maxmind.com/',
     install_requires=['python-geoip'],
     include_package_data=True,
     zip_safe=False,
